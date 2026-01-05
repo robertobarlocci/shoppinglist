c1252132d57f8164afa5331976c28943bbbd08f8
Roberto Barlocci <25400887+robertobarlocci@users.noreply.github.com>
2026-01-05 14:47:34 +0100

    Fix(js): Remove manual X-CSRF-TOKEN setting to fix stale token 419 errors

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,11 +5,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['Accept'] = 'application/json';
 window.axios.defaults.withCredentials = true;
 
-// CSRF token
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+// Axios automatically handles the X-XSRF-TOKEN cookie from Laravel
+// We don't need to manually set the X-CSRF-TOKEN header from the meta tag
+// as that can become stale in an SPA/Inertia app.
